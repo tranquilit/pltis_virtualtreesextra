2e0695079425da4c28a126fa04af48710426db26
htouvet <htouvet-github@tranquil.it>
2018-04-09 16:19:14 +0200

    Renamed to pltis_virtualtreesextra Disabled Rtti grid

---
+++ pltis_virtualtreesextra.pas
@@ -0,0 +1,27 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit pltis_virtualtreesextra;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+      AllVTExtraRegister, vte_buttontree, vte_collectionvirtualtreemediator, 
+      vte_comboeditlink, vte_configproviter, vte_controller, vte_dbcheckgroup, 
+      vte_dbgrid, vte_dbtree, vte_dbtreeex, vte_ddbtreeview, 
+      vte_ddbtreeviewutils, vte_edittree, vte_edittree_editors, vte_initree, 
+      vte_json, vte_nodeinterfaces, vte_objecttree, vte_propertytree, 
+      vte_stringlist, vte_treedata, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+  RegisterUnit('AllVTExtraRegister', @AllVTExtraRegister.Register);
+end;
+
+initialization
+  RegisterPackage('pltis_virtualtreesextra', @Register);
+end.
